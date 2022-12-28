e4495fd297b2e51d7326daf95c5ad51fcb64bc26
Millicherry <ahanacookie@gmail.com>
2022-12-28 11:58:11 +0530

    maven project

---
+++ src/main/java/com/se/mavensample/App.java
@@ -0,0 +1,13 @@
+package com.se.mavensample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Inspiration does exists, but it must find your working" );
+    }
+}
